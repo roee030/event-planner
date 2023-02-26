85856193135c4465b35d806ec5901e705f8c32ca
Roee030 <roee.angel93@gmail.com>
2023-02-26 15:02:52 +0200

    working on the guest page

--- src/components/GuestEventPage.jsx
+++ src/components/GuestEventPage.jsx
@@ -1,6 +1,4 @@
-import React, { useState } from "react";
-import Product from "./Product";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { useState, useEffect } from "react";
 import {
     Paper,
     Typography,
@@ -8,11 +6,10 @@ import {
     FormGroup,
     Button,
     Grid,
-    Container,
     IconButton,
-    Box,
 } from "@material-ui/core";
 import { Add as AddIcon, Remove as RemoveIcon } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -40,56 +37,47 @@ const GuestEventPage = ({ event }) => {
     const classes = useStyles();
     const [selectedProducts, setSelectedProducts] = useState({});
 
+    useEffect(() => {
+        if (event) {
+            const tempSelectedProducts = {};
+            event.products.forEach((product) => {
+                tempSelectedProducts[product.id] = 0;
+            });
+            setSelectedProducts(tempSelectedProducts);
+        }
+    }, [event]);
+
+    const isMaxQuantityReached = (productId, quantity) => {
+        const product = event.products.find((product) => product.id === productId);
+        const selectedQuantity = selectedProducts[productId] || 0;
+        return selectedQuantity + quantity > product.quantity;
+    };
+
+
     const handleProductChange = (productId, quantity) => {
-        setSelectedProducts((prevSelectedProducts) => ({
-            ...prevSelectedProducts,
+        setSelectedProducts((prevState) => ({
+            ...prevState,
             [productId]: quantity,
         }));
     };
 
-    const handleProductIncrement = (productId) => {
-        const newQuantity = (selectedProducts[productId] || 0) + 1;
-        handleProductChange(productId, newQuantity);
-    };
-
-    const handleProductDecrement = (productId) => {
-        const currentQuantity = selectedProducts[productId] || 0;
-        if (currentQuantity > 0) {
-            const newQuantity = currentQuantity - 1;
-            handleProductChange(productId, newQuantity);
-        }
-    };
-
-    const handleSubmit = () => {
-        let totalPrice = 0;
-        for (const product of event.products) {
+    const calculateTotalPrice = (products, selectedProducts) => {
+        let totalPrice = Object.values(products).reduce((acc, product) => {
+            const { price } = product;
             const selectedQuantity = selectedProducts[product.id] || 0;
-            totalPrice += selectedQuantity * product.price;
-        }
-        const totalQuantity = Object.values(selectedProducts).reduce(
-            (acc, val) => acc + val,
-            0
-        );
-
-        const maxGuests = Math.floor(totalPrice / (event.price * 1.15));
-
-        if (totalQuantity > maxGuests) {
-            alert(
-                "Sorry, you can't bring all the products you selected. Please remove some products or reduce the quantity."
-            );
-            return;
-        }
-
-        const attendees = Object.entries(selectedProducts).map(([id, quantity]) => ({
-            id,
-            quantity,
-        }));
-        alert(
-            `Thank you for your RSVP! You have selected ${totalQuantity} items for a total of $${totalPrice.toFixed(
-                2
-            )}. We look forward to seeing you at the event!`
-        );
+            return acc + (price * selectedQuantity);
+        }, 0);
+        return totalPrice.toFixed(2);
     };
+
+
+    const handleRSVP = () => {
+        // calculate total price
+        const totalPrice = calculateTotalPrice(event.products, selectedProducts);
+        console.log("RSVP clicked", selectedProducts, totalPrice);
+    };
+
+
 
     return (
         <Paper className={classes.root}>
@@ -114,50 +102,76 @@ const GuestEventPage = ({ event }) => {
 
             <FormControl component="fieldset" className={classes.formControl}>
                 <FormGroup>
-                    {event.products.map((product) => (
-                        <Grid container key={product.id} alignItems="center" justifyContent="space-between">
-                            <Grid item xs={6}>
-                                <Typography variant="subtitle1">{product.name}</Typography>
+                    {event.products.map((product) => {
+                        const { id, name, quantity } = product;
+                        const selectedQuantity = selectedProducts[id] || 0;
+                        const isMaxQuantityReached = selectedQuantity >= quantity;
+                        const isMinQuantityReached = selectedQuantity <= 0;
+
+                        return (
+                            <Grid
+                                container
+                                key={id}
+                                alignItems="center"
+                                justifyContent="space-between"
+                            >
+                                <Grid item xs={6}>
+                                    <Typography variant="subtitle1">{name}</Typography>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <div style={{ display: "flex", alignItems: "center" }}>
+                                        <IconButton
+                                            aria-label="reduce"
+                                            onClick={() => {
+                                                handleProductChange(id, selectedQuantity - 1);
+                                            }}
+                                            disabled={isMinQuantityReached}
+                                        >
+                                            <RemoveIcon fontSize="small" />
+                                        </IconButton>
+                                        <Typography variant="body1">{selectedQuantity}</Typography>
+                                        <IconButton
+                                            aria-label="increase"
+                                            onClick={() => {
+                                                handleProductChange(id, selectedQuantity + 1);
+                                            }}
+                                            disabled={isMaxQuantityReached}
+
+                                        >
+                                            <AddIcon fontSize="small" />
+                                        </IconButton>
+                                    </div>
+                                </Grid>
+                                {isMaxQuantityReached && (
+                                    <Grid item xs={12}>
+                                        <Typography variant="caption" color="error">
+                                            Not enough {name} available.
+                                        </Typography>
+                                    </Grid>
+                                )}
                             </Grid>
-                            <Grid item xs={6}>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <IconButton
-                                        aria-label="reduce"
-                                        onClick={() => {
-                                            const newQuantity = selectedProducts[product.id] - 1 || 0;
-                                            handleProductChange(product.id, newQuantity);
-                                        }}
-                                    >
-                                        <RemoveIcon fontSize="small" />
-                                    </IconButton>
-                                    <Typography variant="body1">{selectedProducts[product.id] || 0}</Typography>
-                                    <IconButton
-                                        aria-label="increase"
-                                        onClick={() => {
-                                            const newQuantity = (selectedProducts[product.id] || 0) + 1;
-                                            handleProductChange(product.id, newQuantity);
-                                        }}
-                                    >
-                                        <AddIcon fontSize="small" />
-                                    </IconButton>
-                                </div>
-                            </Grid>
-                        </Grid>
-                    ))}
+                        );
+                    })}
+                    <Grid item xs={12}>
+                        <Typography variant="subtitle1" align="right">
+                            Total: ${calculateTotalPrice(event.products, selectedProducts)}
+                        </Typography>
+                    </Grid>
                 </FormGroup>
             </FormControl>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
+
+            <div style={{ display: "flex", justifyContent: "center" }}>
                 <Button
                     variant="contained"
                     color="primary"
                     className={classes.submitBtn}
-                    onClick={handleSubmit}
+                    onClick={handleRSVP}
                 >
                     RSVP
                 </Button>
             </div>
-        </Paper >
+        </Paper>
     );
-};
 
+};
 export default GuestEventPage;
--- src/route/Event.jsx
+++ src/route/Event.jsx
@@ -6,19 +6,19 @@ const Event = () => {
         id: 1,
         title: "My Party",
         date: new Date("2023-03-01T12:00:00"),
+        numberOfGuests: 6,
         location: "123 Main St, Anytown, USA",
-        price: 5,
         products: [
-            { id: 1, name: "Chips", price: 5 },
-            { id: 2, name: "Soda", price: 5 },
-            { id: 3, name: "Pizza", price: 10 },
-            { id: 4, name: "Beer", price: 3 },
+            { id: 1, name: "Chips", price: 5, quantity: 10 },
+            { id: 2, name: "Soda", price: 5, quantity: 2 },
+            { id: 3, name: "Pizza", price: 10, quantity: 6 },
+            { id: 4, name: "Beer", price: 3, quantity: 8 },
         ],
         attendees: [
-            { id: 1, name: "John", products: { 1: 2, 3: 1 } },
-            { id: 2, name: "Bob", products: { 1: 1, 2: 2, 3: 1, 4: 3 } },
-            { id: 3, name: "Alice", products: { 1: 3, 2: 1 } },
-        ],
+            { id: 1, name: "John", products: [{ productId: 1, quantity: 2 }, { productId: 3, quantity: 1 }] },
+            { id: 2, name: "Bob", products: [{ productId: 2, quantity: 1 }, { productId: 2, quantity: 2 }, { productId: 3, quantity: 1 }, { productId: 4, quantity: 3 }] },
+            { id: 3, name: "Alice", products: [{ productId: 3, quantity: 3 }, { productId: 2, quantity: 1 }] },
+        ]
     };
 
 
@@ -28,3 +28,4 @@ const Event = () => {
 };
 
 export default Event;
+// also to add a nice UI in the same line of the product to have an circles of the each attendee the bring the specific product and if we hover the circle
